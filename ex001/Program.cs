8f72ba754ccd8f9ec8036a750e451a6e52222b64
Egor <ee3068316@gmail.com>
2022-09-17 14:07:25 +0300

    add ex1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿System.Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Math.Pow(numA, numB));
+
+
