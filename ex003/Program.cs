455621444d603638f0c66e0398c17f4ae3112a44
Egor <ee3068316@gmail.com>
2022-09-18 13:59:53 +0300

    add ex3

---
+++ ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿void New_array(int[] arr)
+{
+    int[] array = new int[8];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 100);
+    }
+}
+New_array(array);
